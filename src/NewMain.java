21e953dc95efba9d711fb6ba541f03bb5baa03a9
mcvildand <anmc0001@stud.kea.dk>
2024-03-04 09:39:25 +0100

    B test 3

--- src/NewMain.java
+++ src/NewMain.java
@@ -3,6 +3,8 @@ public class NewMain {
     public static void main(String[] args) {
         System.out.println("Deltager B version 1");
         System.out.println("Deltager B version 2");
+        System.out.println("Deltager B version 3");
+
 
     }
 }
