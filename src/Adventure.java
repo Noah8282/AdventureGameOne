94fbf0a5cdd74196c6ac618cef12649de2c0487c
Noah8282 <nova0001@stud.kea.dk>
2024-03-04 11:33:30 +0100

    Adventure v1, Creation of UserInterface (Empty class). Creation and coding of Adventure Class attributes. Complete creation of Room Class.

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,4 +1,32 @@
 public class Adventure {
 
+    ////////// ATTRIBUTES //////////
+    Room room1;
+    Room room2;
+    Room room3;
+    Room room4;
+    Room room5;
+    Room room6;
+    Room room7;
+    Room room8;
+    Room room9;
+    Room currentRoom;
 
+    
+    public Adventure() {
+        //Create rooms
+        room1 = new Room("Room 1","");
+        room2 = new Room("Room 2","");
+        room3 = new Room("Room 3","");
+        room4 = new Room("Room 4","");
+        room5 = new Room("Room 5","");
+        room6 = new Room("Room 6","");
+        room7 = new Room("Room 7","");
+        room8 = new Room("Room 8","");
+        room9 = new Room("Room 9","");
+
+        currentRoom = room1;
+
+
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,43 @@
 public class Room {
+
+    ////////// ATTRIBUTES //////////
+    //Name of the room
+    private String name;
+    //Description of room and rooms contents.
+    private String description;
+
+    //The 4 neighbouring rooms. If null then there is no room to that orientation.
+    private Room east;
+    private Room west;
+    private Room north;
+    private Room south;
+
+
+    ////////// CONSTRUCTOR //////////
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+        east = null;
+        west = null;
+        north = null;
+        south = null;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+
 }
