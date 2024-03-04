2ac17d97ea89bc60ac04ab54355fe4685d721f68
Salem <salo0002@stud.kea.dk>
2024-03-04 09:38:40 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hej World");
         System.out.println("Yo World");
+        System.out.println("Bonjour World");
     }
 }
