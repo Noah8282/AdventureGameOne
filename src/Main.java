9a61f29a48ec3e664c1e2e898078a39771f95519
Salem <salo0002@stud.kea.dk>
2024-03-04 10:01:48 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
         System.out.println("N");
         System.out.println("O");
         System.out.println("NAH");
+        System.out.println("Lebron");
+        System.out.println("is the goat!");
+
     }
 }
