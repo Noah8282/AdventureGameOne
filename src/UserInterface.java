d662f48e59e13a83e75f85b5429b2775da5ffee3
Noah8282 <nova0001@stud.kea.dk>
2024-03-05 14:57:21 +0100

    Implementation of menu method. Inserted menu method in Adventure constructor, to call the menu first time code is runned.

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -7,12 +7,14 @@ public class UserInterface {
     String input;
     Adventure adventure;
 
+
     //CONSTRUCTOR//
     public UserInterface() {
         scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
         print("Game has started");
         adventure = new Adventure();
+        menu();
         startUI();
 
     }
@@ -27,15 +29,7 @@ public class UserInterface {
                 case "go south","south","s" -> print(adventure.goDirection("s"));
                 case "go west","west","w" -> print(adventure.goDirection("w"));
                 case "go east","east","e" -> print(adventure.goDirection("e"));
-                case "help" -> System.out.println("""
-                                ////////// Instructions & Commands //////////
-                                Help: Get a list of instructions and commands
-                                Look: Get name and description of the room you are in.
-                                Go North/North/N: Go the the room in the northern direction
-                                Go South/South/s: Go the the room in the southern direction
-                                Go West/West/W: Go the the room in the western direction
-                                Go East/East/E: Go the the room in the northern direction
-                                Exit: Exit the game/program.""");
+                case "help" -> menu();
                 case "look" -> print(adventure.look());
                 default -> {
                     if(!input.equals("exit")) {
@@ -52,6 +46,19 @@ public class UserInterface {
     //Print method. Does System.out.println. Method to avoid doing sout every print statement.
     private void print(String print) {
         System.out.println(print);
+    }
+
+    private void menu() {
+        print("""
+              ////////// Instructions & Commands //////////
+              Help: Get a list of instructions and commands
+              Look: Get name and description of the room you are in.
+              Go North/North/N: Go the the room in the northern direction
+              Go South/South/s: Go the the room in the southern direction
+              Go West/West/W: Go the the room in the western direction
+              Go East/East/E: Go the the room in the northern direction
+              Exit: Exit the game/program.
+              """);
     }
 
 
