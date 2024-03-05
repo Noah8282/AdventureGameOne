d9b9d1610e1bcd00ec2d9689239e12d4b3e1777c
Noah8282 <nova0001@stud.kea.dk>
2024-03-05 14:53:36 +0100

    Implementation of CreateRooms and setRoomConnections methods. Implemented to refactor the constructor.

--- src/Adventure.java
+++ src/Adventure.java
@@ -15,35 +15,11 @@ public class Adventure {
 
     public Adventure() {
         //Create rooms
-        room1 = new Room("Room 1", "Room with no distinct features, except two doors");
-        room2 = new Room("Room 2", "Bright Room with two doors");
-        room3 = new Room("Room 3", "A lightly lit room, with two doors");
-        room4 = new Room("Room 4", "A very dark room, with two doors");
-        room5 = new Room("Room 5", "A very quiet room, with two door");
-        room6 = new Room("Room 6", "A very smelly room, with two doors");
-        room7 = new Room("Room 7", "A slightly dark room, with two doors");
-        room8 = new Room("Room 8", "A room with no distinct feature, that has two doors");
-        room9 = new Room("Room 9", "A lightly lit room, with two doors");
-
+        createRooms();
+        setRoomConnections();
         currentRoom = room1;
 
-        room1.setEast(room2);
-        room1.setSouth(room2);
-        room2.setWest(room1);
-        room2.setEast(room3);
-        room3.setWest(room2);
-        room3.setSouth(room6);
-        room6.setNorth(room3);
-        room6.setSouth(room9);
-        room9.setNorth(room6);
-        room9.setWest(room8);
-        room8.setNorth(room5);
-        room5.setSouth(room8);
-        room8.setWest(room7);
-        room7.setNorth(room4);
-        room7.setEast(room8);
-        room4.setSouth(room7);
-        room4.setNorth(room1);
+
     }
 
 
@@ -65,6 +41,37 @@ public class Adventure {
             return "you cannot go that way";
         }
         return look();
+    }
+
+    private void createRooms() {
+        room1 = new Room("Room 1", "Room with no distinct features, except two doors");
+        room2 = new Room("Room 2", "Bright Room with two doors");
+        room3 = new Room("Room 3", "A lightly lit room, with two doors");
+        room4 = new Room("Room 4", "A very dark room, with two doors");
+        room5 = new Room("Room 5", "A very quiet room, with two door");
+        room6 = new Room("Room 6", "A very smelly room, with two doors");
+        room7 = new Room("Room 7", "A slightly dark room, with two doors");
+        room8 = new Room("Room 8", "A room with no distinct feature, that has two doors");
+        room9 = new Room("Room 9", "A lightly lit room, with two doors");
+    }
+    private void setRoomConnections() {
+        room1.setEast(room2);
+        room1.setSouth(room2);
+        room2.setWest(room1);
+        room2.setEast(room3);
+        room3.setWest(room2);
+        room3.setSouth(room6);
+        room6.setNorth(room3);
+        room6.setSouth(room9);
+        room9.setNorth(room6);
+        room9.setWest(room8);
+        room8.setNorth(room5);
+        room5.setSouth(room8);
+        room8.setWest(room7);
+        room7.setNorth(room4);
+        room7.setEast(room8);
+        room4.setSouth(room7);
+        room4.setNorth(room1);
     }
 
 }
