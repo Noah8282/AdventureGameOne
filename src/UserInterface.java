7472de0e033510f09aff89b684c45644bbd11beb
Noah8282 <nova0001@stud.kea.dk>
2024-03-05 14:35:15 +0100

    UI interface updated with concentation.

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -29,15 +29,15 @@ public class UserInterface {
                 case "go south","south","s" -> System.out.println(adventure.goDirection("s"));
                 case "go west","west","w" -> System.out.println(adventure.goDirection("w"));
                 case "go east","east","e" -> System.out.println(adventure.goDirection("e"));
-                case "help" -> System.out.println(
-                        "////////// Instructions & Commands //////////\n" +
-                        "Help: Get a list of instructions and commands\n" +
-                        "Look: Get name and description of the room you are in.\n" +
-                        "Go North/North/N: Go the the room in the northern direction\n" +
-                        "Go South/South/s: Go the the room in the southern direction\n" +
-                        "Go West/West/W: Go the the room in the western direction\n" +
-                        "Go East/East/E: Go the the room in the northern direction\n" +
-                        "Exit: Exit the game/program.");
+                case "help" -> System.out.println("""
+                                ////////// Instructions & Commands //////////
+                                Help: Get a list of instructions and commands
+                                Look: Get name and description of the room you are in.
+                                Go North/North/N: Go the the room in the northern direction
+                                Go South/South/s: Go the the room in the southern direction
+                                Go West/West/W: Go the the room in the western direction
+                                Go East/East/E: Go the the room in the northern direction
+                                Exit: Exit the game/program.""");
                 case "look" -> System.out.println(adventure.look());
                 default -> {
                     if(!input.equals("exit")) {
