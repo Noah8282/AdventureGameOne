f4e40bdccbed8f1514b83a9dee9cd2b12f32de62
Noah8282 <nova0001@stud.kea.dk>
2024-03-15 09:43:42 +0100

    Added comments to AudioPlayer and Item class

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -6,16 +6,21 @@ import java.io.InputStream;
 import java.util.HashMap;
 
 public class AudioPlayer {
+    //Sets a string with the default file path
     String filePath = "/resources/audio/audio";
+    //Sets attribute HashMap, that matches audiofilepath with room objects.
     HashMap<Room, String> audioFilePaths;
+    //Defines Clip attribute for later use
     Clip clip;
+    //Defines boolean for disabling and enabling music.
     private boolean musicEnabled = true;
+    //Defines Room object for the current room.
     private Room currentRoom;
 
     public AudioPlayer(Room[] roomList) {
-        // Change this to the path of your WAV file
+        //Instantiates Hashmap
         audioFilePaths = new HashMap<>();
-
+        //Loop for setting and adding each filepath to the value, and setting a room object as a key.
         for (int i = 0; i < roomList.length; i++) {
             audioFilePaths.put(roomList[i], filePath + (i + 1) + ".wav");
         }
@@ -23,24 +28,36 @@ public class AudioPlayer {
 
 
     public void startAudio(Room room) {
+        //Sets players room to this attributes currentroom.
         currentRoom = room;
-
+        //Checks if music is enabled
         if (musicEnabled) {
+            //Gets file from filepath and inserts it into an InputStream.
             try (InputStream is = getClass().getResourceAsStream(audioFilePaths.get(room))) {
                 AudioInputStream ais;
+                //If inputStream is null, it could not fine the filepath. This usually indicates, that the program
+                //build is being run, and not from the IntelliJ project tree.
                 if (is == null) {
+                    //Makes a file Object instead, as this can access files outside this directory, which audio files
+                    //are located when in IntelliJ directory tree.
                     File newFilePath = new File("resources/" + audioFilePaths.get(room));
                     ais = AudioSystem.getAudioInputStream(newFilePath);
                 } else {
+                    //BIS is used instead of File object, to avoid mark and reset compilation Exception
                     BufferedInputStream bis = new BufferedInputStream(is);
                     ais = AudioSystem.getAudioInputStream(bis);
                 }
-
+                //Checks if AudioSystem object is null. If yes then no audiofiles where found.
                 if (ais != null) {
+                    //Create audioformat object to get audiofiles format.
                     AudioFormat format = ais.getFormat();
+                    //Create Dalaline.Info class that inserts the Clip class and the format from AudioFormat.
+                    //Used to get info regarding audiofile for use in AudioSystem, which is downcast into Clip.
                     DataLine.Info info = new DataLine.Info(Clip.class, format);
                     clip = (Clip) AudioSystem.getLine(info);
+                    //Opening the audiofile.
                     clip.open(ais);
+                    //Setting the audiofile to loop, until stopped.
                     clip.loop(Clip.LOOP_CONTINUOUSLY);
 
 
@@ -53,8 +70,13 @@ public class AudioPlayer {
         }
     }
 
+    //Toggles the audio from off to on.
     public void toggleAudio() {
+        //Checks if the clip is null. If yes it has never been instantiated. Usually means the music has been disabled
+        //From the start.
         if (clip != null) {
+            //Checks if music is enabled and if the clip is running. If it is not, check if music is enabled, if it is, then
+            //start the music. If it isn't always try to stop the music, as music is disabled.
             if (musicEnabled && clip.isRunning()) {
                 clip.stop();
             } else if (musicEnabled) {
@@ -62,23 +84,25 @@ public class AudioPlayer {
             } else {
                 clip.stop();
             }
-        } else {
+        } /*else {
             return;
         }
 
         if (clip != null) {
             clip.isRunning();
-        }
+        }*/
     }
 
     public String userToggleMusic() {
+        //Reverts boolean when user wished to toggle the music.
         musicEnabled = !musicEnabled;
+        //If the music has been started, start audio. Else stop the audio.
         if (musicEnabled) {
             startAudio(currentRoom);
         } else {
             toggleAudio();
         }
-
+        //Returns string for userinterface to print.
         return musicEnabled ? "Music has been enabled" : "Music has been disabled";
     }
 
--- src/Item.java
+++ src/Item.java
@@ -3,37 +3,57 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Item {
+    //Description of the item
     private String longName;
+    //Short name for the item (one word)
     private String shortName = "";
+    //A list of possible shortnames, which can be iterated through if no shortname has been provided.
     private ArrayList<String> keywords;
 
     public Item(String shortName, String longName) {
+        //Sets short and long name for manual implementation.
         this.longName = longName;
         this.shortName = shortName;
     }
 
     public Item(String longName) {
+        //Sets longname
         this.longName = longName;
-        keywords = new ArrayList<>(List.of("lamp", "sword", "axe", "bow", "helmet", "chestplate", "leggings", "boots", "chestplate", "bone","steak","pork","apple","beans","potato", "bread", "banana"));
+        //Sets arraylist of possible shortnames/keywords
+        keywords = new ArrayList<>(List.of("lamp", "sword", "axe", "bow", "helmet", "chestplate", "leggings", "boots",
+                "chestplate", "bone","steak","pork","apple","beans","potato", "bread", "banana"));
+        //Takes the long name and splits each word into an array
         String[] wordsInLongName = longName.split(" ");
+        //Iterations through each keyword
         for (String kw : keywords) {
+            //Iteration through each word from the item description/long name
             for (int i = 0; i < wordsInLongName.length; i++) {
+                //Checks if a keyword from the list matches a word from the item description.
+                // If yes it sets the keyword to the short name.
                 if (wordsInLongName[i].equalsIgnoreCase(kw)) {
                     shortName = kw;
+                    break;
+                }
+                //Checks if the shortname isn't blank. If so, break the outer loop. No need to further iterate through.
+                if(!shortName.isBlank()) {
+                    break;
                 }
             }
         }
-
+        //If the loop has done a full iteration through keywords, and shortname has not been set, it is not a valid short name.
+        //Illegal Exception is thrown to encourage the developer to manually insert the short-and longName.
         if (shortName.isBlank()) {
             throw new IllegalStateException("I can't recognise the description. Please define the short- and longName");
         }
 
     }
 
+    //Getter for longName
     public String getLongName() {
         return longName;
     }
 
+    //Getter for shortname.
     public String getShortName() {
         return shortName;
     }
